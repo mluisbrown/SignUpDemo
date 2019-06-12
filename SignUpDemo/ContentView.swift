00ef31a42560e0206925d89e9749c9785433297e
Michael Brown <michael.brown@babylonhealth.com>
2019-06-12 12:04:37 +0100

    Initial Commit

---
+++ SignUpDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SignUpDemo
+//
+//  Created by Michael Brown on 12/06/2019.
+//  Copyright © 2019 Babylon Health. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
