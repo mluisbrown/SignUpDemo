724d82a9c9cd13661d5e5cabf28f952fb19f0df1
Michael Brown <michael.brown@babylonhealth.com>
2019-06-21 00:53:36 +0100

    Stage 2 -> Stage 3 (end) snippets.

--- Snippets.swift
+++ Snippets.swift
@@ -1,3 +1,6 @@
+//---------------------------------------------------------------------------------------------
+// Stage 1 -> Stage 2
+
 enum Action {
     case didChangeEmail(String)
     case didChangePassword(String)
@@ -85,3 +88,99 @@ private static func reduce(state: State, event: Event) -> State {
     }
 }
 
+//---------------------------------------------------------------------------------------------
+// Stage 2 -> Stage 3 (end)
+
+private let input = PassthroughSubject<Event, Never>()
+private var cancelable: Cancellable? = nil
+
+init(
+    api: GravatarAPI = GravatarAPI.live
+) {
+    state = State()
+    cancelable = Publishers.system(
+        initial: state,
+        feedbacks: [
+            SignUpViewModel.whenSigningUp(api: api),
+            Feedback(effects: { _ in self.input.eraseToAnyPublisher() })
+        ],
+        scheduler: DispatchQueue.main,
+        reduce: SignUpViewModel.reduce
+    ).sink { [weak self] state in
+        guard let self = self else { return }
+        dump(state)
+        self.state = state
+        self.didChange.send(())
+    }
+}
+
+deinit {
+    didChange.send(completion: .finished)
+    input.send(completion: .finished)
+    cancelable?.cancel()
+}
+
+private static func whenSigningUp(api: GravatarAPI) -> Feedback<State, Event> {
+    return Feedback(predicate: { $0.isSigningUp }) { state -> AnyPublisher<Event, Never> in
+        return api.getAvatar(state.email)
+            .map(Event.signUpSucceeded)
+            .replaceError(replace: Event.signUpFailed)
+    }
+}
+
+struct GravatarAPI {
+    private static func MD5(string: String) -> String {
+        let digest = Insecure.MD5.hash(data: string.data(using: .utf8) ?? Data())
+
+        return digest.map {
+            String(format: "%02hhx", $0)
+        }.joined()
+    }
+
+    let getAvatar: (String) -> AnyPublisher<UIImage, SignUpError>
+}
+
+extension GravatarAPI {
+    static let live: GravatarAPI = GravatarAPI { email -> AnyPublisher<UIImage, SignUpError> in
+        let emailHash = MD5(string: email.lowercased())
+        let url = URL(string: "https://www.gravatar.com/avatar/\(emailHash)?s=256&d=404")!
+
+        let request = URLRequest(url: url)
+
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { result -> UIImage in
+                let (data, response) = result
+                guard let httpResponse = response as? HTTPURLResponse,
+                    200..<300 ~= httpResponse.statusCode,
+                    let image = UIImage(data: data) else { throw SignUpError.unknown }
+
+                return image
+            }
+            .mapError { error -> SignUpError in
+                switch error.self {
+                case is URLError:
+                    return .network(error)
+                default:
+                    return .unknown
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+
+    static let mockFailure: GravatarAPI = GravatarAPI { email -> AnyPublisher<UIImage, SignUpError> in
+        return Publishers.Once(Result<UIImage, SignUpError>.failure(.unknown))
+            .eraseToAnyPublisher()
+    }
+
+    static let mockSuccess: GravatarAPI = GravatarAPI { email -> AnyPublisher<UIImage, SignUpError> in
+        return Publishers.Once(
+            Result<UIImage, SignUpError>.success(
+                UIImage(
+                    systemName: "person.circle",
+                    withConfiguration: UIImage.SymbolConfiguration.init(pointSize: 100)
+                )!
+            )
+        )
+        .eraseToAnyPublisher()
+    }
+}
